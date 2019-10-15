76d53d928274b5e6863c7cf1108ffe6df1b51584
Edunogodz <edunogodz@gmail.com>
2019-10-15 00:29:54 -0300

    Initial Commit

---
+++ Z2Practice/Z2Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Z2Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
