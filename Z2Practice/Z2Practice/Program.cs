448cbe674c9bbed12af6f2c43127d7a8e98b07f0
Edunogodz <edunogodz@gmail.com>
2020-02-24 21:53:12 -0300

    Z2P C# Tutorial Lesson 8

--- Z2Practice/Z2Practice/Program.cs
+++ Z2Practice/Z2Practice/Program.cs
@@ -1,33 +1,80 @@
 ﻿using System;
+using System.Text;
 
 namespace Z2Practice
 {
     class Program
     {
-        static void Main(string[] args)
+       public static void Main(string[] args)
         {
-            //Animal myAnimal = new Animal();
-            IAnimal myAnimal;
-            Console.WriteLine("What animal are you?");
+            ////Animal myAnimal = new Animal();
+            //IAnimal myAnimal;
+            //Console.WriteLine("What animal are you?");
+            //string input = Console.ReadLine();
+
+            //if (input == "Dog")
+            //{
+            //    myAnimal = new Dog();
+            //}
+            //else
+            //{
+            //    myAnimal = new Animal();
+            //}
+
+            //myAnimal.Name = "Jessica";
+            //myAnimal.Breathe();
+
+            //string exemploLabel =
+            //DateTime.Now.ToString("d") + "\n\n" +
+            //DateTime.Now.ToString("D") + "\n\n" +
+            //DateTime.Now.ToString("t") + "\n\n" +
+            //DateTime.Now.ToString("T") + "\n\n" +
+            //DateTime.Now.ToString("f") + "\n\n" +
+            //DateTime.Now.ToString("F") + "\n\n" +
+            //DateTime.Now.ToString("g") + "\n\n" +
+            //DateTime.Now.ToString("G") + "\n\n" +
+            //DateTime.Now.ToString("M") + "\n\n" +
+            //DateTime.Now.ToString("r") + "\n\n" +
+            //DateTime.Now.ToString("s") + "\n\n" +
+            //DateTime.Now.ToString("u") + "\n\n" +
+            //DateTime.Now.ToString("Y") + "\n\n" +
+            //DateTime.Now.ToString("dd") + "\n\n" +
+            //DateTime.Now.ToString("ddd") + "\n\n" +
+            //DateTime.Now.ToString("dddd") + "\n\n" +
+            //DateTime.Now.ToString("MM") + "\n\n" +
+            //DateTime.Now.ToString("MMM") + "\n\n" +
+            //DateTime.Now.ToString("MMMM") + "\n\n" +
+            //DateTime.Now.ToString("yy") + "\n\n" +
+            //DateTime.Now.ToString("yyyy") + "\n\n" +
+            //DateTime.Now.ToString("dd/MM/yyyy hh:mm") + "\n\n" +
+            //DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.fff") + "\n\n" +
+            //DateTime.Now.ToString("São Paulo, dddd dd 'de' MMMM 'de' yyyy");
+
+            //Console.WriteLine(exemploLabel);
+
             string input = Console.ReadLine();
+            Console.WriteLine(ReverseString(input));
+        }
 
-            if (input == "Dog")
+        public static string ReverseString(string input)
+        {
+           // string reversedString = string.Empty;
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = input.Length - 1; i >= 0; i--)
             {
-                myAnimal = new Dog();
-            }
-            else
-            {
-                myAnimal = new Animal();
+                //reversedString += input[i]; 
+                
+                sb.Append(input[i]);
             }
 
-            myAnimal.Name = "Jessica";
-            myAnimal.Breathe();
+            return sb.ToString();// reversedString;
         }
     }
 
     class Animal : IAnimal
     {
-        public string Name { get;  set ; }
+        public string Name { get; set; }
 
         public string Color { get; set; }
 
@@ -41,7 +88,7 @@ namespace Z2Practice
     {
         public string Name { get; set; }
 
- 
+
         public void Breathe()
         {
             Console.WriteLine("I'm a dog breathing");
@@ -51,10 +98,12 @@ namespace Z2Practice
 
     interface IAnimal
     {
-        string  Name { get; set; }
+        string Name { get; set; }
 
         void Breathe();
-        
+
     }
+
+    
 }
 
